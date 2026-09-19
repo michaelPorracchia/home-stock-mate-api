63348929fa13aa472aa41a2afa4843107e5cd548
PORRACCHIA Michaël <michaelPorracchia@users.noreply.github.com>
2026-09-19 22:35:21 +0200

    Configuration Maven

---
+++ src/main/java/com/homestockmate/home_stock_mate_api/HomeStockMateApiApplication.java
@@ -0,0 +1,13 @@
+package com.homestockmate.home_stock_mate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStockMateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeStockMateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homestockmate/home_stock_mate_api/HomeStockMateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homestockmate.home_stock_mate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStockMateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
